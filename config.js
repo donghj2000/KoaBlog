9744d9c00e5c9669937a05d3dfb20d5c01cb7f7c
donghj2000 <84324399+donghj2000@users.noreply.github.com>
2022-04-12 11:18:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ module.exports = {
 
   EMAIL_SERVER_TYPE: "qq", //类型qq邮箱
   EMAIL_PORT: 587,
-  EMAIL_HOST_USER: "81037981@qq.com", // 发送方的邮箱
-  EMAIL_HOST_PASSWORD: "hlqzyssfrifkbjea", // smtp 的授权码
+  EMAIL_HOST_USER: "xxxxxxxxx@qq.com", // 发送方的邮箱
+  EMAIL_HOST_PASSWORD: "xxxxxxxxxxxxxx", // smtp 的授权码
 
   MYSQL_DB: "koablog",
   MYSQL_USERNAME: "root",
