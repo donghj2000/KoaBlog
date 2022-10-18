e20d50ea66739574dd35402a80ec55be86539cd3
donghj2000 <84324399+donghj2000@users.noreply.github.com>
2022-10-18 12:04:14 +0800

    Update elasticsearch7.js

--- elasticsearch7.js
+++ elasticsearch7.js
@@ -2,16 +2,17 @@ const { Client } = require('@elastic/elasticsearch');
 const client = new Client({ node: 'http://localhost:9200' });
 
 const { Article, Tag, Catalog } = require("./models/blog");
+const constants = require("./models/constants");
 const config = require("./config");
 
 var setmap = {
-	settings: {
-	    index: {
-	        number_of_shards: 1,
-	        number_of_replicas: 0
-	    }
-	},
-	mappings: {
+    settings: {
+        index: {
+            number_of_shards: 1,
+            number_of_replicas: 0
+        }
+    },
+    mappings: {
         properties: {
             "id": {
                 "type": "integer"
@@ -36,17 +37,10 @@ var setmap = {
                 "analyzer": "ik_smart",
                 "type": "text"
             },
-            "category_info": {
-                "properties": {
-                    "name": {
-                        "search_analyzer": "ik_smart",
-                        "analyzer": "ik_smart",
-                        "type": "text"
-                    },
-                    "id": {
-                        "type": "integer"
-                    }
-                }
+            "status": {
+                "search_analyzer": "ik_smart",
+                "analyzer": "ik_smart",
+                "type": "text"
             },
             "views": {
                 "type": "integer"
@@ -70,14 +64,14 @@ var setmap = {
                 }
             }
         }
-	}
+    }
 }
 
 const ESCreateIndex = async () => {
-	var ret = await client.indices.delete({
-		index: config.ELASTICSEARCH_INDEX,
-		ignore_unavailable: true
-	});
+    var ret = await client.indices.delete({
+        index: config.ELASTICSEARCH_INDEX,
+        ignore_unavailable: true
+    });
 
     ret = await client.indices.create({
         index: config.ELASTICSEARCH_INDEX,
@@ -85,141 +79,147 @@ const ESCreateIndex = async () => {
     });
 
     let page      = 1;
-	let page_size = 10;
-	while (true) {
-		let articles = await Article.findAll({
-	        offset:  (page - 1) * page_size,
-	        limit:   page_size,
-	        include: [ Catalog, Tag ]
-	    });
+    let page_size = 10;
+    while (true) {
+        let articles = await Article.findAll({
+            offset:  (page - 1) * page_size,
+            limit:   page_size,
+            include: [ Catalog, Tag ]
+        });
 
-		if (articles == null || articles.length == 0) {
-			break;
-		}
+        if (articles == null || articles.length == 0) {
+            break;
+        }
 
-		for (article of articles) {
-        	let tags_info = article.Tags.map(tag=>{
-        		return {
-					id: 		tag.id,
-					name: 		tag.name }
-			});
-			let catalog_info = {
-				id: 		article.Catalog.id,
-	    		name: 		article.Catalog.name,
-			}
-        	let body = {
-        		id:         article.id,
-            	title: 		article.title,
-            	excerpt:  	article.excerpt,
-            	keyword:    article.keyword,
-            	markdown:   article.markdown,
-            	tags_info:  tags_info,
-            	catalog_info: catalog_info,
-            	views: 		article.views,
-            	comments: 	article.comments,
-            	likes: 		article.likes
-        	};
+        for (article of articles) {
+            let tags_info = article.Tags.map(tag=>{
+                return {
+                    id:         tag.id,
+                    name:       tag.name }
+            });
+            let catalog_info = {
+                id:         article.Catalog.id,
+                name:       article.Catalog.name,
+            }
+            let body = {
+                id:         article.id,
+                title:      article.title,
+                excerpt:    article.excerpt,
+                keyword:    article.keyword,
+                status:     article.status,
+                markdown:   article.markdown,
+                tags_info:  tags_info,
+                catalog_info: catalog_info,
+                views:      article.views,
+                comments:   article.comments,
+                likes:      article.likes
+            };
 
-        	try {
-				ret = await client.index({
-					index: config.ELASTICSEARCH_INDEX, 
-					id: article.id,
-					body: body
-				});
-			} catch (e) {
-				console.log('e=',e);
-			}
+            try {
+                ret = await client.index({
+                    index: config.ELASTICSEARCH_INDEX, 
+                    id: article.id,
+                    body: body
+                });
+            } catch (e) {
+                console.log('e=',e);
+            }
         };
 
-		page++;
-	}
+        page++;
+    }
 }
 
 
 const ESUpdateIndex = async (article) => {
-	if (config.ELASTICSEARCH_ON == false) {
-		return;
-	}
-	let tags_info = article.Tags.map(tag=>{
-		return {
-			id: 	tag.id,
-			name: 	tag.name 
-		}
-	});
-	let catalog_info = {
-		id: 		article.Catalog.id,
-		name: 		article.Catalog.name,
-	}
-	let body = {
-		id:         article.id,
-    	title: 		article.title,
-    	excerpt:  	article.excerpt,
-    	keyword:    article.keyword,
-    	markdown:   article.markdown,
-    	tags_info:  tags_info,
-    	catalog_info: catalog_info,
-    	views: 		article.views,
-    	comments: 	article.comments,
-    	likes: 		article.likes
-	};
+    if (config.ELASTICSEARCH_ON == false) {
+        return;
+    }
+    let tags_info = article.Tags.map(tag=>{
+        return {
+            id:     tag.id,
+            name:   tag.name 
+        }
+    });
+    let catalog_info = {
+        id:         article.Catalog.id,
+        name:       article.Catalog.name,
+    }
+    let body = {
+        id:         article.id,
+        title:      article.title,
+        excerpt:    article.excerpt,
+        keyword:    article.keyword,
+        markdown:   article.markdown,
+        status:     article.status,
+        tags_info:  tags_info,
+        catalog_info: catalog_info,
+        views:      article.views,
+        comments:   article.comments,
+        likes:      article.likes
+    };
 
-	try {
-		ret = await client.index({
-			index:   config.ELASTICSEARCH_INDEX, 
-			refresh: true,
-			id:      article.id,
-			body:    body
-		});
-	} catch (e) {
-		console.log('e=',e);
-	}
+    try {
+        ret = await client.index({
+            index:   config.ELASTICSEARCH_INDEX, 
+            refresh: true,
+            id:      article.id,
+            body:    body
+        });
+    } catch (e) {
+        console.log('e=',e);
+    }
 }
 
 const ESSearchIndex = async (page, page_size, search_text) => {
-	if (config.ELASTICSEARCH_ON == false) {
-		return { count: 0, results: [] };
-	}
-	let ret = null;
-	try {
-		ret = await client.search({
-			index: config.ELASTICSEARCH_INDEX,
-			body: {
-				"query": { 
-			    	"bool": { 
-			      		"should": [
-			        		{ "match": { "title":   		   search_text }}, 
-			        		{ "match": { "excerpt": 		   search_text }},
-			        		{ "match": { "keyword": 		   search_text }}, 
-			        		{ "match": { "markdown": 		   search_text }},
-			        		{ "match": { "tags_info.name":     search_text }},
-			        		{ "match": { "catalog_info.name":  search_text }}, 
-			      		]
-			    	}
-			  	}
-			},
-			"from": (page - 1) * page_size,
-		  	"size": page_size
-		});
-	} catch (e) {
-		console.log(e);
-		ret = null;
-	}
+    if (config.ELASTICSEARCH_ON == false) {
+        return { count: 0, results: [] };
+    }
+    let ret = null;
+    try {
+        ret = await client.search({
+            index: config.ELASTICSEARCH_INDEX,
+            body: {
+                "query": { 
+                    "bool": { 
+                        "should": [
+                            { "match": { "title":              search_text }}, 
+                            { "match": { "excerpt":            search_text }},
+                            { "match": { "keyword":            search_text }}, 
+                            { "match": { "markdown":           search_text }},
+                            { "match": { "tags_info.name":     search_text }},
+                            { "match": { "catalog_info.name":  search_text }}, 
+                        ],
+                        "must" : {
+                            "match" : {
+                                "status": constants.ARTICLE_STATUS_PUBLISHED
+                            }
+                        }
+                    }
+                  }
+            },
+            "from": (page - 1) * page_size,
+            "size": page_size
+        });
+    } catch (e) {
+        console.log(e);
+        ret = null;
+    }
 
-	let articleList= null;
-	if (ret != null) {
-		//console.log('search ret=', ret.body.hits.total,ret.body.hits.hits);
-		if (ret.body.hits.total.value > 0) {
-			articleList = ret.body.hits.hits.map(article=>{
-				return { object: article._source }  //兼容前端
-			});
-			return { count: ret.body.hits.total.value, results: articleList };
-		} 
-	}
-	
-	return { count: 0, results: [] };
+    let articleList= null;
+    if (ret != null) {
+        if (ret.body.hits.total.value > 0) {
+            articleList = ret.body.hits.hits.map(article=>{
+                return { object: article._source }  //兼容前端
+            });
+            return { count: ret.body.hits.total.value, results: articleList };
+        } 
+    }
+    
+    return { count: 0, results: [] };
 }
 module.exports = {
-	ESCreateIndex,
-	ESUpdateIndex,
-	ESSearchIndex
+    ESCreateIndex,
+    ESUpdateIndex,
+    ESSearchIndex
 }
